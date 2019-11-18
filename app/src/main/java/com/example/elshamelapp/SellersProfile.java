ff4835e31b30c7c03c14e7f793bf6587b6b5abff
moh <MohamedElkhouly123>
2019-11-18 04:39:59 +0200

    new

---
+++ app/src/main/java/com/example/elshamelapp/SellersProfile.java
@@ -0,0 +1,14 @@
+package com.example.elshamelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellersProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sellers_profile);
+    }
+}
