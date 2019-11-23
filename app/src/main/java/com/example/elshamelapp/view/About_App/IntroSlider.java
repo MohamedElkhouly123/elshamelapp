fd39cea9a1b5a7b1aeb7c7f628e81c9c4112e445
moh <MohamedElkhouly123>
2019-11-24 01:30:24 +0200

    new

--- app/src/main/java/com/example/elshamelapp/view/About_App/IntroSlider.java
+++ app/src/main/java/com/example/elshamelapp/view/About_App/IntroSlider.java
@@ -22,32 +22,32 @@ import android.widget.TextView;
 import com.example.elshamelapp.R;
 import com.example.elshamelapp.view.Regester.Regester;
 
-public class AboutApp extends AppCompatActivity {
+public class IntroSlider extends AppCompatActivity {
     private ViewPager viewPager;
     private MyViewPagerAdapter myViewPagerAdapter;
     private LinearLayout dotsLayout;
     private TextView[] dots;
     private int[] layouts;
     private Button btnSkip, btnNext;
-//    private PrefManager prefManager;
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Checking for first time launch - before calling setContentView()
-//        prefManager = new PrefManager(this);
-//        if (!prefManager.isFirstTimeLaunch()) {
-//            launchHomeScreen();
-//            finish();
-//        }
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
 
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
-        setContentView(R.layout.activity_about_app);
+        setContentView(R.layout.activity_intro_slider);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
@@ -60,7 +60,6 @@ public class AboutApp extends AppCompatActivity {
         layouts = new int[]{
                 R.layout.welcome_slide1,
                 R.layout.welcome_slide2,
-                R.layout.welcome_slide3,
                 R.layout.welcome_slide4};
 
         // adding bottom dots
@@ -120,8 +119,8 @@ public class AboutApp extends AppCompatActivity {
     }
 
     private void launchHomeScreen() {
-//        prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(AboutApp.this, Regester.class));
+        prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(IntroSlider.this, Regester.class));
         finish();
     }
 
